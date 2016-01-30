3a9d298d2f141cc3449f4bfcfa79a42cb4c94842
jerzynaf <filip.jerzyna"answerconsulting.com>
2016-01-30 18:00:04 +0000

    app.js added.

---
+++ MvcTest/MvcTest.Web/Assets/js/app.js
@@ -0,0 +1,30 @@
+﻿var PeopleManager = new Marionette.Application();
+
+PeopleManager.navigate = function (route, options) {
+  options || (options = {});
+  Backbone.history.navigate(route, options);
+};
+
+PeopleManager.getCurrentRoute = function () {
+  return Backbone.history.fragment;
+};
+
+PeopleManager.on("start", function () {
+  var RegionContainer = Marionette.LayoutView.extend({
+    el: "#app-container",
+
+    regions: {
+      main: "#main-region"
+    }
+  });
+
+  PeopleManager.regions = new RegionContainer();
+
+  if (Backbone.history) {
+    Backbone.history.start();
+
+    if (this.getCurrentRoute() === "") {
+      ContactManager.trigger("contacts:list");
+    }
+  }
+});
