7a7e81eed962318d569094176c4a94707c0f415a
jerzynaf <filip.jerzyna"answerconsulting.com>
2016-01-30 18:06:29 +0000

    Person entity implemented.

---
+++ MvcTest/MvcTest.Web/Assets/js/entities/person.js
@@ -0,0 +1,56 @@
+﻿PeopleManager.module("Entities", function (Entities, PeopleManager, Backbone, Marionette, $, _) {
+  Entities.Person = Backbone.Model.extend({
+    urlRoot: "api/PeopleApi",
+
+    defaults: {
+      firstName: "",
+      lastName: ""
+    }
+  });
+
+  Entities.PeopleCollection = Backbone.Collection.extend({
+    url: "api/PeopleApi",
+    model: Entities.Person
+  });
+
+  var API = {
+    getPeopleEntities: function () {
+      var people = new Entities.PeopleCollection();
+      var defer = $.Deferred();
+      people.fetch({
+        success: function (data) {
+          defer.resolve(data);
+        }
+      });
+      var promise = defer.promise();
+      $.when(promise).done(function (fetchedPeople) {
+        if (fetchedPeople.length === 0) {
+          alert("No people");
+        }
+      });
+      return promise;
+    },
+
+    getPersontEntity: function (personId) {
+      var person = new Entities.Person({ id: personId });
+      var defer = $.Deferred();
+      person.fetch({
+        success: function (data) {
+          defer.resolve(data);
+        },
+        error: function (data) {
+          defer.resolve(undefined);
+        }
+      });
+      return defer.promise();
+    }
+  };
+
+  PeopleManager.reqres.setHandler("person:entities", function () {
+    return API.getPersonEntities();
+  });
+
+  PeopleManager.reqres.setHandler("person:entity", function (id) {
+    return API.getPersonEntity(id);
+  });
+});
