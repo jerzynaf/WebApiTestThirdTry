976ba1a2af59137ad153e859034d15d496810f83
jerzynaf <filip.jerzyna"answerconsulting.com>
2016-01-30 18:21:02 +0000

    peopleApp added.

---
+++ MvcTest/MvcTest.Web/Assets/js/apps/people/people_app.js
@@ -0,0 +1,34 @@
+﻿PeopleManager.module("PeopleApp", function (PeopleApp, PeopleManager, Backbone, Marionette, $, _) {
+  PeopleApp.Router = Marionette.AppRouter.extend({
+    appRoutes: {
+      "people": "listPeople",
+      "people/edit/:id": "editPerson"
+    }
+  });
+
+  var API = {
+    listPeople: function () {
+      PeopleApp.List.Controller.listPeople();
+    },
+
+    editPerson: function (id) {
+      PeopleApp.Edit.Controller.editPerson(id);
+    }
+  };
+
+  PeopleManager.on("people:list", function () {
+    PeopleManager.navigate("people");
+    API.listPeople();
+  });
+
+  PeopleManager.on("person:edit", function (id) {
+    PeopleManager.navigate("people/edit/" + id);
+    API.editPerson(id);
+  });
+
+  PeopleApp.on("start", function () {
+    new PeopleApp.Router({
+      controller: API
+    });
+  });
+});
